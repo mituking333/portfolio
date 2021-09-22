740ab709638fd3dc36f48b5a866ddcc4cb4c2836
mituking333 <87847294+mituking333@users.noreply.github.com>
2021-09-23 06:55:10 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+`use strict`;
+//---------------------------
+//Object.keysでオブジェクトから配列へ
+//---------------------------
+{
+  // 開くのID取得
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  // 閉じるのID取得
+  const close = document.getElementById(`close`);
+  
+
+
+  // メニューをクリックした場合
+  open.addEventListener('click', () => {
+    // メニュー画面を開く
+    overlay.classList.add('show');
+    // ハンバーガー三本を消す
+    open.classList.add('hide');
+  });
+
+
+  // 閉じる押下時
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    // ハンバーガー三本を消す
+    open.classList.remove('hide');
+  });
+
+  // メニューを選択した場合、閉じる押下と同じ処理を実行
+  function myFunction(event) { 
+    overlay.classList.remove('show');
+    // ハンバーガー三本を消す
+    open.classList.remove('hide');
+  }
+  }
+  
